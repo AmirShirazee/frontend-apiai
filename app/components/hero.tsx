f5842820aa8e82e3671d70955b8a50320bf97dac
amirahmadi <ahmadi_soheyl@live.nl>
2024-02-19 21:50:17 +0100

    changes to hero component

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,76 +1,81 @@
-import Image from 'next/image';
-import Particles from './particles';
-import Illustration from '../../public/images/glow-bottom.svg';
+import Image from "next/image";
+import Particles from "./particles";
+import Illustration from "../../public/images/glow-bottom.svg";
 
 export default function Hero() {
   return (
     <section>
-      <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
+      <div className='relative max-w-6xl mx-auto px-4 sm:px-6'>
         {/* Particles animation */}
-        <Particles className="absolute inset-0 -z-10" />
+        <Particles className='absolute inset-0 -z-10' />
 
         {/* Tech-Savvy Illustration */}
         <div
-          className="absolute inset-0 -z-10 -mx-28 rounded-b-[3rem] pointer-events-none overflow-hidden"
-          aria-hidden="true"
+          className='absolute inset-0 -z-10 -mx-28 rounded-b-[3rem] pointer-events-none overflow-hidden'
+          aria-hidden='true'
         >
-          <div className="absolute left-1/2 -translate-x-1/2 bottom-0 -z-10">
+          <div className='absolute left-1/2 -translate-x-1/2 bottom-0 -z-10'>
             <Image
               src={Illustration}
-              className="max-w-none"
+              className='max-w-none'
               width={2146}
               priority
-              alt="Tech Illustration"
+              alt='Tech Illustration'
             />
           </div>
         </div>
 
-        <div className="pt-32 pb-16 md:pt-52 md:pb-32">
+        <div className='pt-32 pb-16 md:pt-52 md:pb-32'>
           {/* Hero content */}
-          <div className="max-w-3xl mx-auto text-center">
-            <div className="mb-6" data-aos="fade-down">
-              <div className="inline-flex relative before:absolute before:inset-0 before:bg-purple-500 before:blur-md">
-                <span className="btn-sm py-0.5 text-slate-300 transition duration-150 ease-in-out group">
-                  <span className="relative inline-flex items-center">
-                    New: Performance test generation using Artillery!
-                  </span>
+          <div className='max-w-3xl mx-auto text-center'>
+            <div className='mb-6' data-aos='fade-down'>
+              <div className='inline-flex relative before:absolute before:inset-0 before:bg-purple-500 before:blur-md'>
+                <span className='btn-sm py-0.5 text-slate-300 transition duration-150 ease-in-out group'>
+                  <span className='relative inline-flex items-center'></span>
                 </span>
               </div>
             </div>
-            <div className="mb-6" data-aos="fade-down">
-              <div className="inline-flex relative before:absolute before:inset-0 before:bg-purple-500 before:blur-md">
-                <a className="btn-sm py-0.5 text-slate-300 hover:text-white transition duration-150 ease-in-out group [background:linear-gradient(theme(colors.purple.500),_theme(colors.purple.500))_padding-box,_linear-gradient(theme(colors.purple.500),_theme(colors.purple.200)_75%,_theme(colors.transparent)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/50 before:rounded-full before:pointer-events-none shadow"
-                   href="#0">
-                  <span className="relative inline-flex items-center">
-                    API Studio is now in beta <span
-                      className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+            <div className='mb-6' data-aos='fade-down'>
+              <div className='inline-flex relative before:absolute before:inset-0 before:bg-purple-500 before:blur-md'>
+                <a
+                  className='btn-sm py-0.5 text-slate-300 hover:text-white transition duration-150 ease-in-out group [background:linear-gradient(theme(colors.purple.500),_theme(colors.purple.500))_padding-box,_linear-gradient(theme(colors.purple.500),_theme(colors.purple.200)_75%,_theme(colors.transparent)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/50 before:rounded-full before:pointer-events-none shadow'
+                  href='#0'
+                >
+                  <span className='relative inline-flex items-center'>
+                    TestOpenApi is now live!
+                    <span className='tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1'>
+                      -&gt;
+                    </span>
                   </span>
                 </a>
               </div>
             </div>
             <h1
-                className="h1 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4"
-                data-aos="fade-down"
+              className='h1 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4'
+              data-aos='fade-down'
             >
               Integration Test Generator
             </h1>
-            <p className="text-lg text-slate-300 mb-8" data-aos="fade-down" data-aos-delay="200">
-              Upload your OpenAPI specification and get Jest integration tests generated instantly.
-              Tailored, tested, and reliable.
+            <p
+              className='text-lg text-slate-300 mb-8'
+              data-aos='fade-down'
+              data-aos-delay='200'
+            >
+              Upload your OpenAPI specification and get Jest integration tests
+              generated instantly. Tailored, tested, and reliable.
             </p>
             <div
-                className="max-w-xs mx-auto sm:max-w-none sm:inline-flex sm:justify-center space-y-4 sm:space-y-0 sm:space-x-4"
-                data-aos="fade-down"
-                data-aos-delay="400"
+              className='max-w-xs mx-auto sm:max-w-none sm:inline-flex sm:justify-center space-y-4 sm:space-y-0 sm:space-x-4'
+              data-aos='fade-down'
+              data-aos-delay='400'
             >
               <div>
                 <a
-                    className="btn text-slate-900 bg-gradient-to-r from-white/80 via-white to-white/80 hover:bg-white w-full transition duration-150 ease-in-out group"
-                    href="/upload"
+                  className='btn text-slate-900 bg-gradient-to-r from-white/80 via-white to-white/80 hover:bg-white w-full transition duration-150 ease-in-out group'
+                  href='/upload'
                 >
-                  Get started{' '}
-                  <span
-                      className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
+                  Get started{" "}
+                  <span className='tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1'>
                     -&gt;
                   </span>
                 </a>
