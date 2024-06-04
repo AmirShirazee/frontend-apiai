dcda1deab35a7ebc113e5b6997c3fc62a224faf4
amir_ahmadi <amir.ahmadi@knmi.nl>
2024-06-04 16:34:46 +0200

    added health route

---
+++ app/api/health/route.ts
@@ -0,0 +1,15 @@
+import type { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  if (req.method !== "GET") {
+    return new Response(JSON.stringify({ message: "Method not allowed" }), {
+      status: 405,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  return new Response(JSON.stringify({ message: "OK!" }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
